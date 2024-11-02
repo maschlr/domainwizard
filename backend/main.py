7f79fc08cd38f73b1c18991db2a793ddfc025aa1
Marten (msc) <msc@odoo.com>
2024-11-02 07:46:54 -0400

    add timestamp to uvicorn logging

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
+import uvicorn
 from domainwizard.config import config
 from domainwizard.routes import app
+from uvicorn.config import LOGGING_CONFIG
 
 if __name__ == "__main__":
-    import uvicorn
-
+    LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s [%(name)s] %(levelprefix)s %(message)s"
     uvicorn.run(app, host="localhost", port=config.get("FASTAPI_PORT", 8000))
